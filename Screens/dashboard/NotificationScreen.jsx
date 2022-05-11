c67aff7bb39fdef7f1f7a3c1277e5260e487476b
Jaay7 <jayakrishna.madem@gmail.com>
2022-05-11 19:15:53 +0530

    Notification screen

---
+++ Screens/dashboard/NotificationScreen.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Text } from '@react-native-material/core'
+import { StatusBar, StyleSheet, View, Platform } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useIsFocused } from '@react-navigation/native';
+
+const NotificationScreen = () => {
+  const [darkTheme, setDarkTheme] = React.useState(false);
+  const isFocused = useIsFocused();
+
+  React.useEffect(() => {
+    const getTheme = async() => {
+      const theme = await AsyncStorage.getItem('theme');
+      if(theme === 'dark') {
+        setDarkTheme(true);
+      } else {
+        setDarkTheme(false);
+      }
+    }
+    if(isFocused) {
+      getTheme();
+    }
+  }, [darkTheme, isFocused]);
+
+  return (
+    <View style={darkTheme ? styles.darkContainer : styles.container}>
+      <Text variant="h6" color={darkTheme ? '#f2f2f2' : '#000'} style={{marginTop: Platform.OS === 'ios' ? 60 : 10}}>Notifications</Text>
+    </View>
+  )
+}
+
+export default NotificationScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: StatusBar.currentHeight
+  },
+  darkContainer: {
+    flex: 1,
+    backgroundColor: '#000',
+    marginTop: StatusBar.currentHeight
+  },
+})
